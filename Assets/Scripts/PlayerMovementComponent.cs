46a6dd2fe97dcf87bb4bd2c1d1970a604f159b57
Veli Emre Çetin <50757076+WeileeTheWeener@users.noreply.github.com>
2023-07-17 15:43:17 +0300

    (feat): added restriction for moving back

--- Assets/Scripts/PlayerMovementComponent.cs
+++ Assets/Scripts/PlayerMovementComponent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovementComponent : MonoBehaviour
 {
     [SerializeField] private Grid grid;
@@ -10,8 +11,11 @@ public class PlayerMovementComponent : MonoBehaviour
     public UnityEvent onMoveForward;
     Vector3Int tileLeftOfPlayer;
     Vector3Int tileRightOfPlayer;
+    Vector3Int tileBehindThePlayer;
     [SerializeField] bool canMoveLeft;
     [SerializeField] bool canMoveRight;
+    [SerializeField] bool canMoveBack;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +26,7 @@ public class PlayerMovementComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CheckIfICanMoveLeftOrRight();
+        CheckIfICanMove();
         GridMovement();
         
     }
@@ -42,44 +46,58 @@ public class PlayerMovementComponent : MonoBehaviour
             gridIndex.y++;
             onMoveForward.Invoke();
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S) && canMoveBack)
         {
             gridIndex.y--;
         }
 
         transform.position = grid.CellToWorld(gridIndex);
     }
-    void CheckIfICanMoveLeftOrRight()
+    void CheckIfICanMove()
     {
         tileLeftOfPlayer = grid.WorldToCell(grid.CellToWorld(new Vector3Int(gridIndex.x - 1, gridIndex.y, gridIndex.z)));
         tileRightOfPlayer = grid.WorldToCell(grid.CellToWorld(new Vector3Int(gridIndex.x + 1, gridIndex.y, gridIndex.z)));
+        tileBehindThePlayer = grid.WorldToCell(grid.CellToWorld(new Vector3Int(gridIndex.x, gridIndex.y-1, gridIndex.z)));
 
         Ray rayLeft = new Ray();
         Ray rayRight = new Ray();
+        Ray rayBehind = new Ray();
 
         rayLeft.origin = grid.CellToWorld(tileLeftOfPlayer);
         rayLeft.direction = Vector2.down;
         rayRight.origin = grid.CellToWorld(tileRightOfPlayer);
         rayRight.direction = Vector2.down;
+        rayBehind.origin = grid.CellToWorld(tileBehindThePlayer);
+        rayBehind.direction = Vector2.down;
 
         RaycastHit hitLeft = new RaycastHit();
         RaycastHit hitRight = new RaycastHit();
+        RaycastHit hitBehind = new RaycastHit();
 
         Debug.DrawRay(rayLeft.origin, rayLeft.direction * 5f,Color.red);
         Debug.DrawRay(rayRight.origin, rayRight.direction * 5f, Color.red);
+        Debug.DrawRay(rayBehind.origin, rayBehind.direction * 5f, Color.red);
 
         if (Physics.Raycast(rayLeft,out hitLeft))
         {
             canMoveLeft = true;
-            Debug.Log("Left: " + hitLeft.transform.name);
+            //Debug.Log("Left: " + hitLeft.transform.name);
         }
         else canMoveLeft = false;
 
         if (Physics.Raycast(rayRight,out hitRight))
         {
             canMoveRight = true;
-            Debug.Log("Right: "+hitRight.transform.name);
+            //Debug.Log("Right: "+hitRight.transform.name);
         }
         else canMoveRight = false;
+
+        if (Physics.Raycast(rayBehind, out hitBehind))
+        {
+            canMoveBack = true;
+            //Debug.Log("Right: "+hitRight.transform.name);
+        }
+        else canMoveBack = false;
+
     }
 }
